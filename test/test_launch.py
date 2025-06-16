1fa655a321b174c4aee9ce936a0aa2ce13b04d18
Chikurin1014 <chikurin1014@gmail.com>
2025-06-16 23:30:49 +0000

    style: :art: format python script

--- test/test_launch.py
+++ test/test_launch.py
@@ -3,9 +3,7 @@ import unittest
 
 from ament_index_python.packages import get_package_share_directory
 
-from controller_manager.hardware_spawner import (
-    is_hardware_component_loaded
-)
+from controller_manager.hardware_spawner import is_hardware_component_loaded
 from controller_manager.test_utils import (
     check_controllers_running,
     check_node_running,
@@ -44,23 +42,24 @@ CONTROLLERS = [
 
 @pytest.mark.rostest
 def generate_test_description():
-    return LaunchDescription([
-        IncludeLaunchDescription(
-            FrontendLaunchDescriptionSource(
-                os.path.join(
-                    get_package_share_directory(PACKAGE_NAME),
-                    'launch',
-                    'launch.xml'
-                )
+    return LaunchDescription(
+        [
+            IncludeLaunchDescription(
+                FrontendLaunchDescriptionSource(
+                    os.path.join(
+                        get_package_share_directory(PACKAGE_NAME),
+                        'launch',
+                        'launch.xml',
+                    )
+                ),
+                launch_arguments={}.items(),
             ),
-            launch_arguments={}.items()
-        ),
-        ReadyToTest(),
-    ])
+            ReadyToTest(),
+        ]
+    )
 
 
 class TestFixture(unittest.TestCase):
-
     @classmethod
     def setUpClass(cls):
         rclpy.init()
@@ -80,11 +79,9 @@ class TestFixture(unittest.TestCase):
             with self.subTest(component=component):
                 self.assertTrue(
                     is_hardware_component_loaded(
-                        self.node,
-                        'controller_manager',
-                        component,
-                        2.0),
-                    f"Hardware component '{component}' is not loaded."
+                        self.node, 'controller_manager', component, 2.0
+                    ),
+                    f"Hardware component '{component}' is not loaded.",
                 )
 
     def test_node_running(self, proc_output):
@@ -96,7 +93,6 @@ class TestFixture(unittest.TestCase):
 
 @launch_testing.post_shutdown_test()
 class TestShutdown(unittest.TestCase):
-
     def test_exit_codes(self, proc_info):
         """Check that all processes exited cleanly."""
         launch_testing.asserts.assertExitCodes(proc_info)
