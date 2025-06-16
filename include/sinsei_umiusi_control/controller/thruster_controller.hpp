7fcdd51d3b2ce18b63a484354c18c27a6ce5e1df
Chikurin1014 <chikurin1014@gmail.com>
2025-06-16 13:59:28 +0000

    style: :adhesive_bandage: 本質と関係ない部分をrevert

--- include/sinsei_umiusi_control/controller/thruster_controller.hpp
+++ include/sinsei_umiusi_control/controller/thruster_controller.hpp
@@ -54,8 +54,7 @@ class ThrusterController : public controller_interface::ChainableControllerInter
     auto on_export_state_interfaces() -> std::vector<hardware_interface::StateInterface> override;
     auto on_set_chained_mode(bool chained_mode) -> bool override;
     auto update_reference_from_subscribers(
-        const rclcpp::Time & time, const rclcpp::Duration & period)
-        -> controller_interface::return_type override;
+        const rclcpp::Time & time, const rclcpp::Duration & period) -> controller_interface::return_type override;
     auto update_and_write_commands(const rclcpp::Time & time, const rclcpp::Duration & period)
         -> controller_interface::return_type override;
 };
