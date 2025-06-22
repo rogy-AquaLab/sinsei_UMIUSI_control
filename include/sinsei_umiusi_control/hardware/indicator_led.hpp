25e7bd1ab3b4d04b16e63a99e3a5119990efa2f6
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2025-06-22 15:21:48 +0900

    feat: pigpioを追加し`indicator_led`の処理を実装

--- include/sinsei_umiusi_control/hardware/indicator_led.hpp
+++ include/sinsei_umiusi_control/hardware/indicator_led.hpp
@@ -1,10 +1,6 @@
 #ifndef SINSEI_UMIUSI_CONTROL_HARDWARE_INDICATOR_LED_HPP
 #define SINSEI_UMIUSI_CONTROL_HARDWARE_INDICATOR_LED_HPP
 
-#include <memory>
-#include <string>
-#include <vector>
-
 #include "hardware_interface/system_interface.hpp"
 #include "rclcpp/macros.hpp"
 #include "sinsei_umiusi_control/cmd/indicator_led.hpp"
@@ -15,14 +11,17 @@ namespace sinsei_umiusi_control::hardware {
 
 class IndicatorLed : public hardware_interface::SystemInterface {
   private:
-    // TODO: 不要なためコメントアウト中。将来的に削除する。
-    // suc::cmd::indicator_led::Enabled enabled;
+    static constexpr int INDICATOR_LED_GPIO = 24;
+
+    bool gpio_initialized = false;
 
   public:
     RCLCPP_SHARED_PTR_DEFINITIONS(IndicatorLed)
 
     IndicatorLed() = default;
 
+    auto on_init(const hardware_interface::HardwareInfo & info)
+        -> hardware_interface::CallbackReturn override;
     auto read(const rclcpp::Time & time, const rclcpp::Duration & period)
         -> hardware_interface::return_type override;
     auto write(const rclcpp::Time & time, const rclcpp::Duration & period)
--- src/sinsei_umiusi_control/hardware/indicator_led.cpp
+++ src/sinsei_umiusi_control/hardware/indicator_led.cpp
@@ -1,8 +1,22 @@
 #include "sinsei_umiusi_control/hardware/indicator_led.hpp"
 
+#include <pigpio.h>
+
 namespace suchw = sinsei_umiusi_control::hardware;
 namespace hif = hardware_interface;
 namespace rlc = rclcpp_lifecycle;
+
+auto suchw::IndicatorLed::on_init(const hif::HardwareInfo & info) -> hif::CallbackReturn {
+    this->hif::SystemInterface::on_init(info);
+
+    RCLCPP_INFO(get_logger(), "Initializing IndicatorLed hardware interface...");
+    auto status = gpioInitialise();
+    this->gpio_initialized = status >= 0;
+
+    gpioSetMode(INDICATOR_LED_GPIO, PI_OUTPUT);
+
+    return hif::CallbackReturn::SUCCESS;
+}
 
 auto suchw::IndicatorLed::read(const rclcpp::Time & /*time*/, const rclcpp::Duration & /*preiod*/)
     -> hif::return_type {
@@ -11,11 +25,16 @@ auto suchw::IndicatorLed::read(const rclcpp::Time & /*time*/, const rclcpp::Dura
 
 auto suchw::IndicatorLed::write(const rclcpp::Time & /*time*/, const rclcpp::Duration & /*period*/)
     -> hif::return_type {
-    // TODO: 以下はデバッグ用。実際はLEDの点灯処理に変更する
+    if (!this->gpio_initialized) {
+        // GPIOが使えない場合（Docker内など）は何もしない
+        return hif::return_type::OK;
+    }
+
     double enabled_raw = get_command("indicator_led/indicator_led/enabled");
     auto enabled =
         *reinterpret_cast<sinsei_umiusi_control::cmd::indicator_led::Enabled *>(&enabled_raw);
-    RCLCPP_INFO(get_logger(), enabled.value ? "true" : "false");
+    gpioWrite(INDICATOR_LED_GPIO, enabled.value ? 1 : 0);
+
     return hif::return_type::OK;
 }
 
