4fc20f6684a978b45c4a26dfa96258cb847abb26
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2025-06-27 13:53:38 +0900

    feat(headlights): 実際に点灯させる処理を追加

---
+++ include/sinsei_umiusi_control/hardware_model/headlights_model.hpp
@@ -0,0 +1,32 @@
+#ifndef SINSEI_UMIUSI_CONTROL_hardware_model_HEADLIGHTS_MODEL_HPP
+#define SINSEI_UMIUSI_CONTROL_hardware_model_HEADLIGHTS_MODEL_HPP
+
+#include <memory>
+
+#include "sinsei_umiusi_control/cmd/headlights.hpp"
+#include "sinsei_umiusi_control/util/gpio.hpp"
+
+namespace suc = sinsei_umiusi_control;
+
+namespace sinsei_umiusi_control::hardware_model {
+
+class HeadlightsModel {
+  private:
+    std::unique_ptr<util::Gpio> high_beam_gpio;
+    std::unique_ptr<util::Gpio> low_beam_gpio;
+    std::unique_ptr<util::Gpio> ir_gpio;
+
+  public:
+    HeadlightsModel(
+        std::unique_ptr<util::Gpio> high_beam_gpio, std::unique_ptr<util::Gpio> low_beam_gpio,
+        std::unique_ptr<util::Gpio> ir_gpio);
+    auto on_read() -> void;
+    auto on_write(
+        sinsei_umiusi_control::cmd::headlights::HighBeamEnabled & high_beam_enabled,
+        sinsei_umiusi_control::cmd::headlights::LowBeamEnabled & low_beam_enabled,
+        sinsei_umiusi_control::cmd::headlights::IrEnabled & ir_enabled) -> void;
+};
+
+}  // namespace sinsei_umiusi_control::hardware_model
+
+#endif  // SINSEI_UMIUSI_CONTROL_hardware_model_HEADLIGHTS_MODEL_HPP
---
+++ src/sinsei_umiusi_control/hardware_model/headlights_model.cpp
@@ -0,0 +1,26 @@
+#include "sinsei_umiusi_control/hardware_model/headlights_model.hpp"
+
+#include <memory>
+
+#include "sinsei_umiusi_control/util/gpio.hpp"
+
+namespace suc = sinsei_umiusi_control;
+namespace suchm = suc::hardware_model;
+
+suchm::HeadlightsModel::HeadlightsModel(
+    std::unique_ptr<suc::util::Gpio> high_beam_gpio, std::unique_ptr<suc::util::Gpio> low_beam_gpio,
+    std::unique_ptr<suc::util::Gpio> ir_gpio)
+: high_beam_gpio(std::move(high_beam_gpio)),
+  low_beam_gpio(std::move(low_beam_gpio)),
+  ir_gpio(std::move(ir_gpio)) {}
+
+auto suchm::HeadlightsModel::on_read() -> void {}
+
+auto suchm::HeadlightsModel::on_write(
+    sinsei_umiusi_control::cmd::headlights::HighBeamEnabled & high_beam_enabled,
+    sinsei_umiusi_control::cmd::headlights::LowBeamEnabled & low_beam_enabled,
+    sinsei_umiusi_control::cmd::headlights::IrEnabled & ir_enabled) -> void {
+    this->high_beam_gpio->write_digital(high_beam_enabled.value);
+    this->low_beam_gpio->write_digital(low_beam_enabled.value);
+    this->ir_gpio->write_digital(ir_enabled.value);
+}
