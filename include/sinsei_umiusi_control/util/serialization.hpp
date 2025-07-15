0ef7dbbae1c70dc0c49d55df9f9502b634edfd33
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2025-07-15 21:48:18 +0900

    refactor(serialization): `NewType` -> `T`

--- include/sinsei_umiusi_control/util/serialization.hpp
+++ include/sinsei_umiusi_control/util/serialization.hpp
@@ -8,33 +8,33 @@ namespace sinsei_umiusi_control::util {
 
 using InterfaceData = double;
 
-template <typename NewType>
-inline auto from_interface_data(InterfaceData & value) -> NewType {
+template <typename T>
+inline auto from_interface_data(InterfaceData & value) -> T {
     static_assert(
-        sizeof(NewType) <= sizeof(InterfaceData),
-        "NewType must be smaller than or equal to InterfaceData (double)");
-    static_assert(std::is_trivially_copyable<NewType>::value, "NewType must be trivially copyable");
-    NewType result;
-    std::memcpy(&result, &value, sizeof(NewType));
+        sizeof(T) <= sizeof(InterfaceData),
+        "T must be smaller than or equal to InterfaceData (double)");
+    static_assert(std::is_trivially_copyable<T>::value, "T must be trivially copyable");
+    T result;
+    std::memcpy(&result, &value, sizeof(T));
     return result;
 }
 
-template <typename NewType>
-inline auto to_interface_data(const NewType & value) -> InterfaceData {
+template <typename T>
+inline auto to_interface_data(const T & value) -> InterfaceData {
     static_assert(
-        sizeof(NewType) <= sizeof(InterfaceData),
-        "NewType must be smaller than or equal to InterfaceData (double)");
-    static_assert(std::is_trivially_copyable<NewType>::value, "NewType must be trivially copyable");
+        sizeof(T) <= sizeof(InterfaceData),
+        "T must be smaller than or equal to InterfaceData (double)");
+    static_assert(std::is_trivially_copyable<T>::value, "T must be trivially copyable");
     InterfaceData result;
     std::memcpy(&result, &value, sizeof(InterfaceData));
     return result;
 }
 
-template <typename NewType>
-inline auto to_interface_data_ptr(NewType & value) -> InterfaceData * {
+template <typename T>
+inline auto to_interface_data_ptr(T & value) -> InterfaceData * {
     static_assert(
-        sizeof(NewType) <= sizeof(InterfaceData),
-        "NewType must be smaller than or equal to InterfaceData (double)");
+        sizeof(T) <= sizeof(InterfaceData),
+        "T must be smaller than or equal to InterfaceData (double)");
     return reinterpret_cast<InterfaceData *>(&value);
 }
 
