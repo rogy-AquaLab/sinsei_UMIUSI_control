93acdad4af2b6450d6a6352ed17627e9b18e6dcd
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2025-07-20 01:08:08 +0900

    refactor(cmd): remove unused `include`

--- include/sinsei_umiusi_control/cmd/thruster.hpp
+++ include/sinsei_umiusi_control/cmd/thruster.hpp
@@ -1,8 +1,6 @@
 #ifndef SINSEI_UMIUSI_CONTROL_CMD_THRUSTER_HPP
 #define SINSEI_UMIUSI_CONTROL_CMD_THRUSTER_HPP
 
-#include <cstdint>
-
 namespace sinsei_umiusi_control::cmd::thruster {
 
 struct ServoEnabled {
