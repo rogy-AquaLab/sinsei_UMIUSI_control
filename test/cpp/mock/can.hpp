c9ae037ffdbc9ed26c9608f3446982d0349bdc4e
Chikurin1014 <chikurin1014@gmail.com>
2025-07-24 12:32:55 +0000

    feat(test): :white_check_mark: add test for can_model

---
+++ test/cpp/mock/can.hpp
@@ -0,0 +1,25 @@
+#ifndef SINSEI_UMIUSI_CONTROL_test_cpp_mock_CAN_HPP
+#define SINSEI_UMIUSI_CONTROL_test_cpp_mock_CAN_HPP
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <rcpputils/tl_expected/expected.hpp>
+
+#include "sinsei_umiusi_control/util/can_interface.hpp"
+
+namespace sinsei_umiusi_control::test::mock {
+
+class Can : public sinsei_umiusi_control::util::CanInterface {
+  public:
+    MOCK_METHOD1(init, tl::expected<void, std::string>(const std::string ifname));
+    MOCK_METHOD0(close, tl::expected<void, std::string>());
+    MOCK_METHOD1(
+        send_frame,
+        tl::expected<void, std::string>(sinsei_umiusi_control::util::CanFrame && frame));
+    MOCK_METHOD0(recv_frame, tl::expected<sinsei_umiusi_control::util::CanFrame, std::string>());
+};
+
+}  // namespace sinsei_umiusi_control::test::mock
+
+#endif  // SINSEI_UMIUSI_CONTROL_test_cpp_mock_CAN_HPP
