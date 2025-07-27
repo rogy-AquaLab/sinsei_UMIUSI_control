756cef5c9d947b3ea11ac8502b589a85bddf2dea
Chikurin1014 <chikurin1014@gmail.com>
2025-07-27 05:41:10 +0000

    refactor(can): :recycle: 一回のループにつき一回だけ`send_frame`が呼ばれるように変更

--- src/sinsei_umiusi_control/hardware/can.cpp
+++ src/sinsei_umiusi_control/hardware/can.cpp
@@ -1,5 +1,6 @@
 #include "sinsei_umiusi_control/hardware/can.hpp"
 
+#include "sinsei_umiusi_control/cmd/main_power.hpp"
 #include "sinsei_umiusi_control/cmd/thruster.hpp"
 #include "sinsei_umiusi_control/hardware_model/impl/linux_can.hpp"
 #include "sinsei_umiusi_control/util/params.hpp"
@@ -141,44 +142,6 @@ auto suchw::Can::read(const rclcpp::Time & /*time*/, const rclcpp::Duration & /*
 
 auto suchw::Can::write(const rclcpp::Time & /*time*/, const rclcpp::Duration & /*period*/)
     -> hif::return_type {
-    auto is_can = this->thruster_mode == util::ThrusterMode::Can;
-
-    std::array<suc::cmd::thruster::EscEnabled, 4> thruster_esc_enabled_cmd;
-    std::array<suc::cmd::thruster::ServoEnabled, 4> thruster_servo_enabled_cmd;
-    std::array<suc::cmd::thruster::Angle, 4> thruster_angle_cmd;
-    std::array<suc::cmd::thruster::DutyCycle, 4> thruster_duty_cycle_cmd;
-
-    if (is_can) {
-        for (size_t i = 0; i < 4; ++i) {
-            auto thruster_name = "thruster" + std::to_string(i + 1);
-            auto esc_enabled_raw = this->get_command(thruster_name + "/esc/enabled");
-            auto servo_enabled_raw = this->get_command(thruster_name + "/servo/enabled");
-            auto angle_raw = this->get_command(thruster_name + "/servo/angle");
-            auto duty_cycle_raw = this->get_command(thruster_name + "/esc/duty_cycle");
-
-            thruster_esc_enabled_cmd[i] =
-                util::from_interface_data<sinsei_umiusi_control::cmd::thruster::EscEnabled>(
-                    esc_enabled_raw);
-            thruster_servo_enabled_cmd[i] =
-                util::from_interface_data<sinsei_umiusi_control::cmd::thruster::ServoEnabled>(
-                    servo_enabled_raw);
-            thruster_angle_cmd[i] =
-                util::from_interface_data<sinsei_umiusi_control::cmd::thruster::Angle>(angle_raw);
-            thruster_duty_cycle_cmd[i] =
-                util::from_interface_data<sinsei_umiusi_control::cmd::thruster::DutyCycle>(
-                    duty_cycle_raw);
-        }
-    }
-
-    auto main_power_enabled = this->get_command("main_power/enabled");
-    auto led_tape_color = this->get_command("led_tape/color");
-
-    auto main_power_enabled_cmd =
-        util::from_interface_data<sinsei_umiusi_control::cmd::main_power::Enabled>(
-            main_power_enabled);
-    auto led_tape_color_cmd =
-        util::from_interface_data<sinsei_umiusi_control::cmd::led_tape::Color>(led_tape_color);
-
     if (!this->model) {
         constexpr auto DURATION = 3000;  // ms
         RCLCPP_WARN_THROTTLE(
@@ -186,18 +149,72 @@ auto suchw::Can::write(const rclcpp::Time & /*time*/, const rclcpp::Duration & /
         return hif::return_type::OK;
     }
 
-    auto res = is_can
-                   ? this->model->on_write(
-                         thruster_esc_enabled_cmd, thruster_servo_enabled_cmd, thruster_angle_cmd,
-                         thruster_duty_cycle_cmd, main_power_enabled_cmd, led_tape_color_cmd)
-                   : this->model->on_write(main_power_enabled_cmd, led_tape_color_cmd);
-    if (!res) {
-        constexpr auto DURATION = 3000;  // ms
-        RCLCPP_ERROR_THROTTLE(
-            this->get_logger(), *this->get_clock(), DURATION, "\n  Failed to write Can: %s",
-            res.error().c_str());
-    }
+    auto _main_power_enabled_raw = this->get_command("main_power/enabled");
+    auto _led_tape_color_raw = this->get_command("led_tape/color");
 
+    auto && main_power_enabled =
+        util::from_interface_data<cmd::main_power::Enabled>(_main_power_enabled_raw);
+    auto && led_tape_color = util::from_interface_data<cmd::led_tape::Color>(_led_tape_color_raw);
+
+    switch (this->thruster_mode) {
+        case util::ThrusterMode::Can: {
+            auto thruster_name = [](size_t i) { return "thruster" + std::to_string(i + 1); };
+
+            auto && thruster_esc_enabled = std::array<cmd::thruster::EscEnabled, 4>{};
+            auto && thruster_servo_enabled = std::array<cmd::thruster::ServoEnabled, 4>{};
+            auto && thruster_duty_cycle = std::array<cmd::thruster::DutyCycle, 4>{};
+            auto && thruster_angle = std::array<cmd::thruster::Angle, 4>{};
+
+            for (size_t i = 0; i < 4; ++i) {
+                auto _thruster_esc_enabled_raw =
+                    this->get_command(thruster_name(i) + "/esc/enabled");
+                auto _thruster_servo_enabled_raw =
+                    this->get_command(thruster_name(i) + "/servo/enabled");
+                auto _thruster_duty_cycle_raw =
+                    this->get_command(thruster_name(i) + "/esc/duty_cycle");
+                auto _thruster_angle_raw = this->get_command(thruster_name(i) + "/servo/angle");
+
+                thruster_esc_enabled[i] =
+                    util::from_interface_data<cmd::thruster::EscEnabled>(_thruster_esc_enabled_raw);
+                thruster_servo_enabled[i] = util::from_interface_data<cmd::thruster::ServoEnabled>(
+                    _thruster_servo_enabled_raw);
+                thruster_duty_cycle[i] =
+                    util::from_interface_data<cmd::thruster::DutyCycle>(_thruster_duty_cycle_raw);
+                thruster_angle[i] =
+                    util::from_interface_data<cmd::thruster::Angle>(_thruster_angle_raw);
+            }
+
+            const auto res = this->model->on_write(
+                std::move(main_power_enabled), std::move(thruster_esc_enabled),
+                std::move(thruster_servo_enabled), std::move(thruster_duty_cycle),
+                std::move(thruster_angle), std::move(led_tape_color));
+            if (!res) {
+                constexpr auto DURATION = 3000;  // ms
+                RCLCPP_ERROR_THROTTLE(
+                    this->get_logger(), *this->get_clock(), DURATION, "\n  Failed to write Can: %s",
+                    res.error().c_str());
+                return hif::return_type::OK;
+            }
+            break;
+        }
+
+        case util::ThrusterMode::Direct: {  // this->thruster_mode == util::ThrusterMode::Direct
+            const auto res =
+                this->model->on_write(std::move(main_power_enabled), std::move(led_tape_color));
+            if (!res) {
+                constexpr auto DURATION = 3000;  // ms
+                RCLCPP_ERROR_THROTTLE(
+                    this->get_logger(), *this->get_clock(), DURATION, "\n  Failed to write Can: %s",
+                    res.error().c_str());
+                return hif::return_type::OK;
+            }
+            break;
+        }
+
+        default: {
+            return hif::return_type::ERROR;  // unreachable
+        }
+    }
     return hif::return_type::OK;
 }
 
--- src/sinsei_umiusi_control/hardware_model/can_model.cpp
+++ src/sinsei_umiusi_control/hardware_model/can_model.cpp
@@ -1,12 +1,63 @@
 #include "sinsei_umiusi_control/hardware_model/can_model.hpp"
 
+#include <rcpputils/tl_expected/expected.hpp>
 #include <string>
 
-#include "sinsei_umiusi_control/cmd/thruster.hpp"
 #include "sinsei_umiusi_control/state/thruster.hpp"
 
 namespace suc = sinsei_umiusi_control;
 namespace suchm = suc::hardware_model;
+
+auto suchm::CanModel::update_and_generate_command(
+    bool is_can_mode, cmd::main_power::Enabled main_power_enabled,
+    std::array<cmd::thruster::EscEnabled, 4> thruster_esc_enabled,
+    std::array<cmd::thruster::ServoEnabled, 4> thruster_servo_enabled,
+    std::array<cmd::thruster::DutyCycle, 4> thruster_duty_cycle,
+    std::array<cmd::thruster::Angle, 4> thruster_angle,
+    cmd::led_tape::Color led_tape_color) -> std::optional<WriteCommand> {
+    {  // main_power/enabled
+        if (this->main_power_enabled.value != main_power_enabled.value) {
+            this->main_power_enabled = std::move(main_power_enabled);
+            return this->main_power_enabled;
+        }
+    }
+    // CANモードの場合のみ、各スラスターのコマンドを扱う
+    if (is_can_mode) {
+        for (size_t i = 0; i < 4; ++i) {  // thrusterN/esc/enbled
+            if (this->thruster_esc_enabled[i].value != thruster_esc_enabled[i].value) {
+                this->thruster_esc_enabled[i] = std::move(thruster_esc_enabled[i]);
+                return std::make_pair(i, std::move(this->thruster_esc_enabled[i]));
+            }
+        }
+        for (size_t i = 0; i < 4; ++i) {  // thrusterN/servo/enabled
+            if (this->thruster_servo_enabled[i].value != thruster_servo_enabled[i].value) {
+                this->thruster_servo_enabled[i] = std::move(thruster_servo_enabled[i]);
+                return std::make_pair(i, std::move(this->thruster_servo_enabled[i]));
+            }
+        }
+        for (size_t i = 0; i < 4; ++i) {  // thrusterN/esc/duty_cycle
+            if (this->thruster_duty_cycle[i].value != thruster_duty_cycle[i].value) {
+                this->thruster_duty_cycle[i] = std::move(thruster_duty_cycle[i]);
+                return std::make_pair(i, std::move(this->thruster_duty_cycle[i]));
+            }
+        }
+        for (size_t i = 0; i < 4; ++i) {  // thrusterN/servo/angle
+            if (this->thruster_angle[i].value != thruster_angle[i].value) {
+                this->thruster_angle[i] = std::move(thruster_angle[i]);
+                return std::make_pair(i, std::move(this->thruster_angle[i]));
+            }
+        }
+    }
+    {  // led_tape/color
+        if ((this->led_tape_color.red != led_tape_color.red) ||
+            (this->led_tape_color.green != led_tape_color.green) ||
+            (this->led_tape_color.blue != led_tape_color.blue)) {
+            this->led_tape_color = std::move(led_tape_color);
+            return this->led_tape_color;
+        }
+    }
+    return std::nullopt;
+}
 
 suchm::CanModel::CanModel(std::shared_ptr<suchm::interface::Can> can, std::array<int, 4> vesc_ids)
 : can(std::move(can)),
@@ -15,7 +66,13 @@ suchm::CanModel::CanModel(std::shared_ptr<suchm::interface::Can> can, std::array
       can::VescModel(vesc_ids[1]),
       can::VescModel(vesc_ids[2]),
       can::VescModel(vesc_ids[3]),
-  }} {}
+  }},
+  main_power_enabled{false},
+  thruster_esc_enabled{{{false}, {false}, {false}, {false}}},
+  thruster_servo_enabled{{{false}, {false}, {false}, {false}}},
+  thruster_duty_cycle{{{0.0}, {0.0}, {0.0}, {0.0}}},
+  thruster_angle{{{0.0}, {0.0}, {0.0}, {0.0}}},
+  led_tape_color{0u, 0u, 0u} {}
 
 auto suchm::CanModel::on_init() -> tl::expected<void, std::string> {
     auto res = this->can->init("can0");
@@ -87,49 +144,103 @@ auto suchm::CanModel::on_read()
 }
 
 auto suchm::CanModel::on_write(
-    std::array<suc::cmd::thruster::EscEnabled, 4> /*thruster_esc_enabled*/,
-    std::array<suc::cmd::thruster::ServoEnabled, 4> /*thruster_servo_enabled*/,
-    std::array<suc::cmd::thruster::Angle, 4> thruster_angle,
-    std::array<suc::cmd::thruster::DutyCycle, 4> thruster_duty_cycle,
-    suc::cmd::main_power::Enabled main_power_enabled,
-    suc::cmd::led_tape::Color led_tape_color) -> tl::expected<void, std::string> {
-    for (size_t i = 0; i < 4; ++i) {
-        // TODO: `thruster_esc_enabled`と`thruster_servo_enabled`の処理を実装する
+    cmd::main_power::Enabled && main_power_enabled,
+    std::array<cmd::thruster::EscEnabled, 4> && thruster_esc_enabled,
+    std::array<cmd::thruster::ServoEnabled, 4> && thruster_servo_enabled,
+    std::array<cmd::thruster::DutyCycle, 4> && thruster_duty_cycle,
+    std::array<cmd::thruster::Angle, 4> && thruster_angle,
+    cmd::led_tape::Color && led_tape_color) -> tl::expected<void, std::string> {
+    auto command_opt = this->update_and_generate_command(
+        true, std::move(main_power_enabled), std::move(thruster_esc_enabled),
+        std::move(thruster_servo_enabled), std::move(thruster_duty_cycle),
+        std::move(thruster_angle), std::move(led_tape_color));
+    if (!command_opt) {
+        // No command to write
+        return {};
+    }
+    auto && command = std::move(command_opt.value());
 
-        auto duty = thruster_duty_cycle[i].value;
-        auto duty_frame = this->vesc_models[i].make_duty_frame(duty);
-        if (!duty_frame) {
-            return tl::make_unexpected(
-                "Failed to create duty frame for thruster " + std::to_string(i + 1) + ": " +
-                duty_frame.error());
+    auto && frame = suchm::interface::CanFrame{};
+
+    switch (command.index()) {
+        case 0: {  // suc::cmd::main_power::Enabled
+            auto & main_power_enabled = std::get<0>(command);
+
+            // TODO: `main_power_enabled`の処理を実装する
+            auto _ = main_power_enabled;
+            return tl::make_unexpected("Not implemented for main power enabled command");
         }
 
-        auto duty_send_res = this->can->send_frame(std::move(duty_frame.value()));
-        if (!duty_send_res) {
-            return tl::make_unexpected(
-                "Failed to send duty frame for thruster " + std::to_string(i + 1) + ": " +
-                duty_send_res.error());
+        case 1: {  // std::pair<ThrusterId, EscEnabled>
+            auto & [id, esc_enabled] = std::get<1>(command);
+            if (id > vesc_models.size()) {
+                return tl::make_unexpected("Invalid thruster ID: " + std::to_string(id));
+            }
+
+            // TODO: `esc_enabled`の処理を実装する
+            auto _ = esc_enabled;
+            return tl::make_unexpected("Not implemented for ESC enabled command");
         }
 
-        auto angle = thruster_angle[i].value;
-        auto angle_frame = this->vesc_models[i].make_servo_angle_frame(angle);
-        if (!angle_frame) {
-            return tl::make_unexpected(
-                "Failed to create servo angle frame for thruster " + std::to_string(i + 1) + ": " +
-                angle_frame.error());
+        case 2: {  // std::pair<ThrusterId, ServoEnabled>
+            auto & [id, servo_enabled] = std::get<2>(command);
+            if (id > vesc_models.size()) {
+                return tl::make_unexpected("Invalid thruster ID: " + std::to_string(id));
+            }
+
+            // TODO: `servo_enabled`の処理を実装する
+            auto _ = servo_enabled;
+            return tl::make_unexpected("Not implemented for servo enabled command");
         }
 
-        auto angle_send_res = this->can->send_frame(std::move(angle_frame.value()));
-        if (!angle_send_res) {
-            return tl::make_unexpected(
-                "Failed to send servo angle frame for thruster " + std::to_string(i + 1) + ": " +
-                angle_send_res.error());
+        case 3: {  // std::pair<ThrusterId, EscDutyCycle>
+            auto & [id, esc_duty_cycle] = std::get<3>(command);
+            if (id > vesc_models.size()) {
+                return tl::make_unexpected("Invalid thruster ID: " + std::to_string(id));
+            }
+
+            auto duty_frame_res = vesc_models[id - 1].make_duty_frame(esc_duty_cycle.value);
+            if (!duty_frame_res) {
+                return tl::make_unexpected(
+                    "Failed to create duty frame for thruster " + std::to_string(id) + ": " +
+                    duty_frame_res.error());
+            }
+            frame = std::move(duty_frame_res.value());
+            break;
+        }
+
+        case 4: {  // std::pair<ThrusterId, ServoAngle>
+            auto & [id, servo_angle] = std::get<4>(command);
+            if (id > vesc_models.size()) {
+                return tl::make_unexpected("Invalid thruster ID: " + std::to_string(id));
+            }
+
+            auto angle_frame_res = vesc_models[id - 1].make_servo_angle_frame(servo_angle.value);
+            if (!angle_frame_res) {
+                return tl::make_unexpected(
+                    "Failed to create servo angle frame for thruster " + std::to_string(id) + ": " +
+                    angle_frame_res.error());
+            }
+            frame = std::move(angle_frame_res.value());
+            break;
+        }
+
+        case 5: {  // suc::cmd::led_tape::Color
+            auto & led_tape_color = std::get<5>(command);
+
+            // TODO: `led_tape_color`の処理を実装する
+            auto _ = led_tape_color;
+            return tl::make_unexpected("Not implemented for LED tape color command");
+        }
+        default: {
+            return tl::make_unexpected("Unknown command type in CanModel::on_write");
         }
     }
-
-    auto write_res = this->on_write(main_power_enabled, led_tape_color);
-    if (!write_res) {
-        return tl::make_unexpected("Failed to write main power and LED tape: " + write_res.error());
+    auto res = this->can->send_frame(std::move(frame));
+    if (!res) {
+        return tl::make_unexpected(
+            "Failed to send CAN frame (command type id: " + std::to_string(command.index()) +
+            "): " + res.error());
     }
     return {};
 }
