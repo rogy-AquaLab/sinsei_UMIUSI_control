96624401a485d490925991672e1ba87364d50c8b
Chikurin1014 <chikurin1014@gmail.com>
2025-07-30 04:38:03 +0000

    refactor(indicator_led_model): :recycle: refactor indicator led model

--- src/sinsei_umiusi_control/hardware_model/indicator_led_model.cpp
+++ src/sinsei_umiusi_control/hardware_model/indicator_led_model.cpp
@@ -8,7 +8,7 @@ namespace suc = sinsei_umiusi_control;
 namespace suchm = suc::hardware_model;
 
 suchm::IndicatorLedModel::IndicatorLedModel(
-    std::unique_ptr<suchm::interface::Gpio> gpio, interface::GpioPin led_pin)
+    std::unique_ptr<suchm::interface::Gpio> gpio, interface::Gpio::Pin led_pin)
 : gpio(std::move(gpio)), led_pin(led_pin) {}
 
 auto suchm::IndicatorLedModel::on_init() -> tl::expected<void, std::string> {
@@ -16,10 +16,10 @@ auto suchm::IndicatorLedModel::on_init() -> tl::expected<void, std::string> {
         .map_error(suchm::interface::gpio_error_to_string);
 }
 
-auto suchm::IndicatorLedModel::on_read() -> tl::expected<void, std::string> { return {}; }
+auto suchm::IndicatorLedModel::on_read() const -> tl::expected<void, std::string> { return {}; }
 
-auto suchm::IndicatorLedModel::on_write(sinsei_umiusi_control::cmd::indicator_led::Enabled &
+auto suchm::IndicatorLedModel::on_write(sinsei_umiusi_control::cmd::indicator_led::Enabled &&
                                             enabled) -> tl::expected<void, std::string> {
-    return this->gpio->write_digital(this->led_pin, enabled.value)
+    return this->gpio->write_digital(this->led_pin, std::move(enabled.value))
         .map_error(suchm::interface::gpio_error_to_string);
 }
