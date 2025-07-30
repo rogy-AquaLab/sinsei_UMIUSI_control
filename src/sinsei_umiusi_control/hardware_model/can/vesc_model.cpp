359ab9faa3ea32e3605ce4487395d035ba7fa3c3
Chikurin1014 <chikurin1014@gmail.com>
2025-07-30 04:06:58 +0000

    refactor(vesc_model): :recycle: refactor vesc model

--- src/sinsei_umiusi_control/hardware_model/can/vesc_model.cpp
+++ src/sinsei_umiusi_control/hardware_model/can/vesc_model.cpp
@@ -3,63 +3,68 @@
 #include "sinsei_umiusi_control/hardware_model/interface/can.hpp"
 #include "sinsei_umiusi_control/util/byte.hpp"
 
-namespace suc = sinsei_umiusi_control;
-namespace suchm = suc::hardware_model;
+using namespace sinsei_umiusi_control::hardware_model;
 
-suchm::can::VescModel::VescModel(suchm::can::VescModel::Id id) : id(id) {}
+can::VescModel::VescModel(can::VescModel::Id id) : id(id) {}
 
-auto suchm::can::VescModel::make_frame(
-    VescSimpleCommandID command_id, const interface::CanFrame::Data & data) -> interface::CanFrame {
-    auto can_id =
+auto can::VescModel::make_frame(
+    VescSimpleCommandID && command_id,
+    interface::CanFrame::Data && data) const -> interface::CanFrame {
+    const auto id =
         (static_cast<interface::CanFrame::Id>(command_id) & 0xFF) << 8 | (this->id & 0xFF);
-    return interface::CanFrame{can_id, 4, data, true};
+    return interface::CanFrame{
+        std::move(id),    // id
+        4,                // len
+        std::move(data),  // data
+        true,             // is_extended
+    };
 }
 
-auto suchm::can::VescModel::make_duty_frame(double duty)
+auto can::VescModel::make_duty_frame(double && duty) const
     -> tl::expected<interface::CanFrame, std::string> {
     if (duty < -1.0 || duty > 1.0) {
         return tl::make_unexpected(
             "Duty must be between -1.0 and 1.0 (duty: " + std::to_string(duty) + ")");
     }
-    auto scaled_duty = static_cast<int32_t>(duty * SET_DUTY_SCALE);
-    auto bytes = suc::util::to_bytes_be(scaled_duty);
-    return this->make_frame(VescSimpleCommandID::CAN_PACKET_SET_DUTY, bytes);
+    auto && scaled_duty = static_cast<int32_t>(std::move(duty) * SET_DUTY_SCALE);
+    auto && bytes = util::to_bytes_be(std::move(scaled_duty));
+    return this->make_frame(VescSimpleCommandID::CAN_PACKET_SET_DUTY, std::move(bytes));
 }
 
-auto suchm::can::VescModel::make_rpm_frame(int8_t rpm)
+auto can::VescModel::make_rpm_frame(int8_t && rpm) const
     -> tl::expected<interface::CanFrame, std::string> {
-    auto scaled_rpm = static_cast<int32_t>(rpm * SET_RPM_SCALE);
-    auto bytes = suc::util::to_bytes_be(scaled_rpm);
-    return this->make_frame(VescSimpleCommandID::CAN_PACKET_SET_RPM, bytes);
+    auto && scaled_rpm = static_cast<int32_t>(std::move(rpm) * SET_RPM_SCALE);
+    auto && bytes = util::to_bytes_be(std::move(scaled_rpm));
+    return this->make_frame(VescSimpleCommandID::CAN_PACKET_SET_RPM, std::move(bytes));
 }
 
-auto suchm::can::VescModel::make_servo_frame(double value)
+auto can::VescModel::make_servo_frame(double && value) const
     -> tl::expected<interface::CanFrame, std::string> {
     if (value < 0.0 || value > 1.0) {
         return tl::make_unexpected(
             "Servo value must be between 0.0 and 1.0 (value: " + std::to_string(value) + ")");
     }
-    auto scaled_value = static_cast<int32_t>(value * SET_SERVO_SCALE);
-    auto bytes = suc::util::to_bytes_be(scaled_value);
-    return this->make_frame(VescSimpleCommandID::CAN_PACKET_SET_SERVO, bytes);
+    auto && scaled_value = static_cast<int32_t>(std::move(value) * SET_SERVO_SCALE);
+    auto && bytes = util::to_bytes_be(std::move(scaled_value));
+    return this->make_frame(VescSimpleCommandID::CAN_PACKET_SET_SERVO, std::move(bytes));
 }
 
-auto suchm::can::VescModel::make_servo_angle_frame(double deg)
+auto can::VescModel::make_servo_angle_frame(double && deg) const
     -> tl::expected<interface::CanFrame, std::string> {
     // 0.0 ~ 180.0度の角度を0.0 ~ 1.0に変換
     if (deg < 0.0 || deg > 180.0) {
         return tl::make_unexpected(
             "Servo angle must be between 0.0 and 180.0 degrees (deg: " + std::to_string(deg) + ")");
     }
-    return this->make_servo_frame(deg / 180.0);
+    return this->make_servo_frame(std::move(deg) / 180.0);
 }
 
-auto suchm::can::VescModel::id_matches(const interface::CanFrame & frame) -> bool {
-    const auto vesc_id = static_cast<suchm::can::VescModel::Id>(frame.id & 0xFF);
+auto can::VescModel::id_matches(const interface::CanFrame & frame) const -> bool {
+    const auto vesc_id = static_cast<can::VescModel::Id>(frame.id & 0xFF);
     return vesc_id == this->id;
 }
 
-auto suchm::can::VescModel::get_cmd_id(const interface::CanFrame & frame)
+auto can::VescModel::get_cmd_id(const interface::CanFrame & frame)
     -> tl::expected<VescStatusCommandID, std::string> {
     if (frame.len != 8) {
         return tl::make_unexpected(
@@ -74,13 +79,13 @@ auto suchm::can::VescModel::get_cmd_id(const interface::CanFrame & frame)
         });
 }
 
-auto suchm::can::VescModel::get_rpm(const interface::CanFrame & frame)
-    -> tl::expected<std::optional<sinsei_umiusi_control::state::thruster::Rpm>, std::string> {
+auto can::VescModel::get_rpm(const interface::CanFrame & frame) const
+    -> tl::expected<std::optional<state::thruster::Rpm>, std::string> {
     if (!this->id_matches(frame)) {
         return std::nullopt;
     }
 
-    auto cmd_id = this->get_cmd_id(frame);
+    const auto cmd_id = this->get_cmd_id(frame);
     if (!cmd_id) {
         return tl::make_unexpected("Failed to get command ID: " + cmd_id.error());
     }
@@ -90,20 +95,20 @@ auto suchm::can::VescModel::get_rpm(const interface::CanFrame & frame)
         return std::nullopt;
     }
 
-    auto scaled_erpm = suc::util::to_int32_be(frame.data);
-    auto erpm = static_cast<double>(scaled_erpm) / ERPM_SCALE;
+    auto && scaled_erpm = util::to_int32_be(frame.data);
+    auto && erpm = static_cast<double>(scaled_erpm) / ERPM_SCALE;
     static constexpr double BLDC_POLE_PAIR = BLDC_POLES / 2.0;
     // ERPMを極対数で割ってRPMに変換
-    return suc::state::thruster::Rpm{erpm / BLDC_POLE_PAIR};
+    return state::thruster::Rpm{erpm / BLDC_POLE_PAIR};
 }
 
-auto suchm::can::VescModel::get_water_leaked(const interface::CanFrame & frame)
-    -> tl::expected<std::optional<suc::state::esc::WaterLeaked>, std::string> {
+auto can::VescModel::get_water_leaked(const interface::CanFrame & frame) const
+    -> tl::expected<std::optional<state::esc::WaterLeaked>, std::string> {
     if (!this->id_matches(frame)) {
         return std::nullopt;
     }
 
-    auto cmd_id = this->get_cmd_id(frame);
+    const auto cmd_id = this->get_cmd_id(frame);
     if (!cmd_id) {
         return tl::make_unexpected("Failed to get command ID: " + cmd_id.error());
     }
@@ -113,8 +118,8 @@ auto suchm::can::VescModel::get_water_leaked(const interface::CanFrame & frame)
         return std::nullopt;
     }
 
-    auto scaled_adc1 = suc::util::to_int16_be(frame.data);
-    auto adc1_voltage = static_cast<double>(scaled_adc1) / ADC1_SCALE;
-    auto water_leaked = adc1_voltage > WATER_LEAKED_VOLTAGE_THRESHOLD;
-    return suc::state::esc::WaterLeaked{water_leaked};
+    auto && scaled_adc1 = util::to_int16_be(frame.data);
+    auto && adc1_voltage = static_cast<double>(scaled_adc1) / ADC1_SCALE;
+    auto && water_leaked = adc1_voltage > WATER_LEAKED_VOLTAGE_THRESHOLD;
+    return state::esc::WaterLeaked{water_leaked};
 }
