569b8ce081892d743d57d911039282e0fd3e4811
Chikurin1014 <chikurin1014@gmail.com>
2025-07-30 04:27:42 +0000

    refactor(headlights_model): :recycle: refactor headlights model

--- src/sinsei_umiusi_control/hardware_model/headlights_model.cpp
+++ src/sinsei_umiusi_control/hardware_model/headlights_model.cpp
@@ -4,45 +4,45 @@
 
 #include "sinsei_umiusi_control/hardware_model/interface/gpio.hpp"
 
-namespace suc = sinsei_umiusi_control;
-namespace suchm = suc::hardware_model;
+using namespace sinsei_umiusi_control::hardware_model;
 
-suchm::HeadlightsModel::HeadlightsModel(
-    std::unique_ptr<suchm::interface::Gpio> gpio,
-    suchm::interface::GpioPin high_beam_pin,  // NOLINT(bugprone-*) TODO: いずれ修正したい
-    suchm::interface::GpioPin low_beam_pin, suchm::interface::GpioPin ir_pin)
+HeadlightsModel::HeadlightsModel(
+    std::unique_ptr<interface::Gpio> gpio,
+    interface::Gpio::Pin high_beam_pin,  // NOLINT(bugprone-*) TODO: いずれ修正したい
+    interface::Gpio::Pin low_beam_pin, interface::Gpio::Pin ir_pin)
 : gpio(std::move(gpio)), high_beam_pin(high_beam_pin), low_beam_pin(low_beam_pin), ir_pin(ir_pin) {}
 
-auto suchm::HeadlightsModel::on_init() -> tl::expected<void, std::string> {
+auto HeadlightsModel::on_init() -> tl::expected<void, std::string> {
     return this->gpio->set_mode_output({this->high_beam_pin, this->low_beam_pin, this->ir_pin})
-        .map_error(suchm::interface::gpio_error_to_string);
+        .map_error(interface::gpio_error_to_string);
 }
 
-auto suchm::HeadlightsModel::on_read() -> tl::expected<void, std::string> { return {}; }
+auto HeadlightsModel::on_read() const -> tl::expected<void, std::string> { return {}; }
 
-auto suchm::HeadlightsModel::on_write(
-    sinsei_umiusi_control::cmd::headlights::HighBeamEnabled & high_beam_enabled,
-    sinsei_umiusi_control::cmd::headlights::LowBeamEnabled & low_beam_enabled,
-    sinsei_umiusi_control::cmd::headlights::IrEnabled & ir_enabled)
-    -> tl::expected<void, std::string> {
-    auto res_high = this->gpio->write_digital(this->high_beam_pin, high_beam_enabled.value)
-                        .map_error(suchm::interface::gpio_error_to_string);
-    auto res_low = this->gpio->write_digital(this->low_beam_pin, low_beam_enabled.value)
-                       .map_error(suchm::interface::gpio_error_to_string);
-    auto res_ir = this->gpio->write_digital(this->ir_pin, ir_enabled.value)
-                      .map_error(suchm::interface::gpio_error_to_string);
+auto HeadlightsModel::on_write(
+    cmd::headlights::HighBeamEnabled && high_beam_enabled,
+    cmd::headlights::LowBeamEnabled && low_beam_enabled,
+    cmd::headlights::IrEnabled && ir_enabled) -> tl::expected<void, std::string> {
+    const auto res_high =
+        this->gpio
+            ->write_digital(std::move(this->high_beam_pin), std::move(high_beam_enabled.value))
+            .map_error(interface::gpio_error_to_string);
+    const auto res_low =
+        this->gpio->write_digital(this->low_beam_pin, std::move(low_beam_enabled.value))
+            .map_error(interface::gpio_error_to_string);
+    const auto res_ir = this->gpio->write_digital(this->ir_pin, std::move(ir_enabled.value))
+                            .map_error(interface::gpio_error_to_string);
 
     if (res_high && res_low && res_ir) {
         return {};
     }
 
-    auto msg_high = res_high ? "Success" : res_high.error();
-    auto msg_low = res_low ? "Success" : res_low.error();
-    auto msg_ir = res_ir ? "Success" : res_ir.error();
+    const auto & msg_high = res_high ? "Success" : res_high.error();
+    const auto & msg_low = res_low ? "Success" : res_low.error();
+    const auto & msg_ir = res_ir ? "Success" : res_ir.error();
 
     // すべてのエラーをまとめて返す
-    auto msg = "Failed to write to GPIO pins:\n    High Beam: " + msg_high +
-               "\n    Low Beam: " + msg_low + "\n    IR: " + msg_ir;
-
-    return tl::unexpected(msg);
+    return tl::unexpected(
+        "Failed to write to GPIO pins:\n    High Beam: " + msg_high + "\n    Low Beam: " + msg_low +
+        "\n    IR: " + msg_ir);
 }
