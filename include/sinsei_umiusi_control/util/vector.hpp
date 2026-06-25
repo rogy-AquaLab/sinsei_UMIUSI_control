39ee1f4e0bae5fb51800a3497ec617f1ef31bb18
Yuta Kato <64204135+yk4to@users.noreply.github.com>
2026-06-25 13:04:41 +0900

    refactor(util): map_vector関数が微妙だったのでやめる

--- include/sinsei_umiusi_control/util/vector.hpp
+++ include/sinsei_umiusi_control/util/vector.hpp
@@ -1,32 +1,19 @@
 #ifndef SINSEI_UMIUSI_CONTROL_UTIL_VECTOR_HPP
 #define SINSEI_UMIUSI_CONTROL_UTIL_VECTOR_HPP
 
-#include <type_traits>
 #include <vector>
 
 namespace sinsei_umiusi_control::util {
 
-// 型Tからconst参照と参照を取り除いた型
-template <typename T>
-using remove_cvref_t = std::remove_cv_t<std::remove_reference_t<T>>;
-
-template <typename From, typename Func>
-inline auto map_vector(const std::vector<From> & from, Func && func) {
-    using Mapped = std::invoke_result_t<Func &, const From &>;
-    using To = remove_cvref_t<Mapped>;
-
-    std::vector<To> to;
-    to.reserve(from.size());
-    for (const auto & value : from) {
-        to.push_back(func(value));
-    }
-    return to;
-}
-
 // std::vectorの要素の型を変換する
 template <typename To, typename From>
 inline auto cast_vector(const std::vector<From> & from) -> std::vector<To> {
-    return map_vector(from, [](const From & value) { return static_cast<To>(value); });
+    std::vector<To> to;
+    to.reserve(from.size());
+    for (const auto & value : from) {
+        to.push_back(static_cast<To>(value));
+    }
+    return to;
 }
 
 }  // namespace sinsei_umiusi_control::util
